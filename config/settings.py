7a13b034b09effb76ec911f453b45fc2c5020954
Luis Garreta <luisgarreta@yahoo.com>
2026-05-15 12:09:24 -0500

    May/15/26: r0.9994 : Added CRUE_RUNTYPE

--- config/settings.py
+++ config/settings.py
@@ -128,9 +128,11 @@ USE_X_FORWARDED_HOST = True
 STATICFILES_DIRS  = []
 
 DEBUG   = True
-RUNTYPE = "LOCAL"  # Run as LOCAL or as "SERVICE"
 
-if RUNTYPE == "LOCAL":
+# Run as LOCAL or as "SERVICE"
+CRUE_RUNTYPE = os.environ.get("CRUE_RUNTYPE", "SERVICE").upper()
+
+if CRUE_RUNTYPE == "LOCAL":
     # ─── Archivos estáticos ──────────────────────────────────────────────────────
     #FORCE_SCRIPT_NAME = '/crue-remisiones'
     STATIC_URL        = '/static/'
